d573d1668a7f750efeb1ad31340f1ffe3e826170
isadora-vargasg <isadora.vargasg@gmail.com>
2023-10-24 08:54:13 -0600

    Interfaz de home

--- src/elements/home.js
+++ src/elements/home.js
@@ -1,20 +1,33 @@
 export function home(navigateTo) {
-    const section = document.createElement('section');
+  // const header1=documnet.createElement ("header")
+  const section = document.createElement('section');
     const title = document.createElement('h2');
-    const button = document.createElement('button');
-    const imgMujeres = document.createElement('img');
-    imgMujeres.setAttribute("class","image1")
-    
-    imgMujeres.src="img/Mujer -Space_-2.png";
-    
-  
-    button.textContent = 'entrar';
-    button.addEventListener('click', () => {
+    const buttonExit = document.createElement('button');
+    buttonExit.textContent = 'Salir';
+    buttonExit.addEventListener('click', () => {
       navigateTo('/login');
     });
-  
-    title.textContent = 'Bienvenidas a Mujer -Space-';
-  
-    section.append(title, button, imgMujeres);
-    return section;
-  }
+    title.textContent = 'Bienvenida a Mujer Space';
+    const imgMujeres1 = document.createElement('img');
+    imgMujeres1.setAttribute("class","image2")
+    imgMujeres1.src="img/MUJERES1.png";
+    const postContainer = document.createElement('div');
+    postContainer.setAttribute('class', 'post-container');
+    const postTitle = document.createElement('input');
+    postTitle.setAttribute('type', 'text');
+    postTitle.setAttribute('class', 'post-title');
+    postTitle.setAttribute('placeholder', '¿Qué nos quieres compartir hoy?');
+    const postContent = document.createElement('textarea');
+    postContent.setAttribute('class', 'post-content');
+    postContent.setAttribute('placeholder', 'Cuentanos aquí');
+    const postButton = document.createElement('button');
+    postButton.setAttribute('class', 'post-button');
+    postButton.textContent = 'Publicar';
+    postButton.addEventListener('click', () => {
+      const title = postTitle.value;
+      const content = postContent.value;
+    });
+
+  postContainer.append( postTitle, postContent, postButton);
+  return postContainer;
+}
