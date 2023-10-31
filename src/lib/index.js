62322725dc6fba7b7a5728a6f1ccb56904e95155
isadora-vargasg <isadora.vargasg@gmail.com>
2023-10-31 16:39:56 -0600

    post ordenados

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,18 @@
 import {
-  db, collection, addDoc, getDocs,
+  db, collection, addDoc, getDocs, onSnapshot, query, orderBy,
 } from '../firestore.js';
 
+const postCollection = collection(db, 'posts');
+
 export const addPost = (comment) => {
-  addDoc(collection(db, 'posts'), {
+  addDoc(postCollection, {
     comment,
+    date: Date.now(),
   });
 };
 
-export const querySnapshot = getDocs(collection(db, 'posts'));
+export const querySnapshot = getDocs(postCollection);
+
+const q = query(postCollection, orderBy('date', 'desc'));
+
+export const paintRealTime = (callback) => onSnapshot(q, callback);
