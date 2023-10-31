fdcf466fee4fdc882e3df3789235e33c7c7f8b63
MarcelaGaravito <mache095@gmail.com>
2023-10-31 08:50:50 -0600

    test3

--- test/login.spec.js
+++ test/login.spec.js
@@ -1,6 +1,11 @@
 // importamos la funcion que vamos a testear
 // import { myFunction } from '../src/lib/index';
+
 import { NewUser, auth } from '../src/auth.js';
+import { register } from '../src/elements/register.js';
+// import * as index from '../src/elements/register.js';
+
+jest.setTimeout(30000); // 10 segundos
 
 describe('NewUser', () => {
   test('is a function', () => {
@@ -9,16 +14,20 @@ describe('NewUser', () => {
 });
 
 describe('button login', () => {
-  test('testing button NewUser', () => {
-    jest.spyOn(auth, NewUser).mockImplementation(() => Promise.resolve({ email: 'hola@hola.com', password: '123456' }));
+  test('testing button NewUser', async () => {
     const DOM = document.createElement('div');
-    DOM.append(NewUser());
+    DOM.append(register());
     const email = DOM.querySelector('#emailR');
     const password = DOM.querySelector('#passwordR');
+    const buttonLogin = DOM.querySelector('#buttonLogin');
     email.value = 'prueba@prueba.com';
     password.value = '123456';
-    const buttonLogin = DOM.querySelector('#buttonLogin');
-    buttonLogin.click();
-    expect(auth.NewUser).toHaveBeenCalledTimes(1);
+    const spyNewUser = jest.spyOn(auth, 'NewUser').mockResolvedValue({
+      email: 'prueba@gmail.com',
+      password: '123456',
+    });
+    await buttonLogin.click();
+    expect(spyNewUser).toHaveBeenCalledTimes(1);
+    spyNewUser.mockRestore();
   });
 });
