46efbee9c83492feb47377401e3ef10580b2af20
MarcelaGaravito <mache095@gmail.com>
2023-11-02 18:13:34 -0600

    cambios

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,3 @@
-
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -87,22 +86,7 @@ export const loginEmail = (email, password) => signInWithEmailAndPassword(auth, 
       // alert(`Ocurrió un error:  ${errorMessage}`);
     }
 
-
-  //------ verificacion-------
-// export const sendSignInLinkToEmail =(email)
-// sendSignInLinkToEmail(auth, email, actionCodeSettings)
-//   .then(() => {
-//     // The link was successfully sent. Inform the user.
-//     // Save the email locally so you don't need to ask the user for it again
-//     // if they open the link on the same device.
-//     window.localStorage.setItem('emailForSignIn', email);
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ...
-//   });
+  });
 
 
   // Registro para ingresar------
@@ -128,36 +112,3 @@ export const loginEmail = (email, password) => signInWithEmailAndPassword(auth, 
         }
       });
     })
-
-
-
-//require---------
-var firebase = require('firebase');
-var firebaseui = require('firebaseui');
-
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-
-//-----Dirección de correo electrónico y contraseña-----
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
-
-
-// ---- Proveedores de OAuth ------
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      // List of OAuth providers supported.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-
-  });
