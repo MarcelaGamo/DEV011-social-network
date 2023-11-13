d597ee70f001aca5becffafb0b2ad0fc43f21ed7
isadora-vargasg <isadora.vargasg@gmail.com>
2023-11-13 10:28:25 -0600

    editPostAct

--- src/elements/home.js
+++ src/elements/home.js
@@ -1,23 +1,20 @@
 // import { async } from 'regenerator-runtime';
 import {
-  addPost, 
-  paintRealTime, 
-  deletePost, 
-  editpost, 
+  addPost,
+  paintRealTime,
+  deletePost,
+  getDocument,
+  editPost,
   likePost,
-  //removeLike
 } from '../lib/index.js';
-
 import { auth } from '../auth.js';
 
 let editingPostId = null;
-
 export function home(navigateTo) {
   const section = document.createElement('section');
-
   const title = document.createElement('h2');
   title.setAttribute('class', 'postitle');
-  title.textContent = 'Bienvenida a MUJER SPACE'
+  title.textContent = 'Bienvenida a MUJER SPACE';
   const buttonExit = document.createElement('button');
   buttonExit.setAttribute('type', 'submit');
   buttonExit.setAttribute('class', 'buttonexit');
@@ -29,12 +26,10 @@ export function home(navigateTo) {
   // imgMujeres1.setAttribute('alt', 'mujer1');
   // imgMujeres1.setAttribute('class', 'image2');
   // imgMujeres1.src = 'img/MUJERES1.png';
-
   const imgMujeres = document.createElement('img');
   imgMujeres.setAttribute('alt', 'mujer');
   imgMujeres.setAttribute('class', 'image2');
   imgMujeres.src = 'img/Mujer -Space_-2.png';
-
   const postContainer = document.createElement('div');
   postContainer.setAttribute('class', 'post-container');
   const postTitle = document.createElement('input');
@@ -43,37 +38,29 @@ export function home(navigateTo) {
   postTitle.setAttribute('class', 'post-title');
   postTitle.setAttribute('id', 'postTitle');
   postTitle.setAttribute('placeholder', '¿Qué nos quieres compartir hoy?');
-
-
-const postButton = document.createElement('button');
-postButton.setAttribute('class', 'post-button');
-postButton.textContent = 'Publicar';
-postButton.addEventListener('click', async () => {
-  const comment = postTitle.value;
-
-  if (editingPostId) {
-    try {
-      await editpost(editingPostId, comment);
-      console.log('Post editado correctamente');
-      paintRealTime((querySnapshot) => {
+  const postButton = document.createElement('button');
+  postButton.setAttribute('class', 'post-button');
+  postButton.textContent = 'Publicar';
+  postButton.addEventListener('click', async () => {
+    const comment = postTitle.value;
+    if (editingPostId) {
+      try {
+        await editPost(editingPostId, comment);
+        console.log('Post editado correctamente');
         editingPostId = null;
-      });
-    } catch (error) {
-      console.error('Error al editar el post', error);
+      } catch (error) {
+        console.error('Error al editar el post', error);
+      }
+    } else {
+      try {
+        await addPost(comment, auth.currentUser.email);
+        console.log('Post agregado correctamente');
+      } catch (error) {
+        console.error('Error al agregar el post', error);
+      }
     }
-  } else {
-    try {
-      await addPost(comment, auth.currentUser.email);
-      console.log('Post agregado correctamente');
-      paintRealTime((querySnapshot) => {
-      });
-    } catch (error) {
-      console.error('Error al agregar el post', error);
-    }
-  }
-  postTitle.value = '';
-});
-
+    postTitle.value = '';
+  });
   const publicationPost = document.createElement('article');
   publicationPost.setAttribute('class', 'post-section');
   paintRealTime((querySnapshot) => {
@@ -81,77 +68,65 @@ postButton.addEventListener('click', async () => {
     querySnapshot.forEach((doc) => {
       const post = document.createElement('div');
       post.classList.add('post-container-in');
-
       post.innerHTML = `
       <div class="post-container1">
         <p class="post-title">${doc.data().comment}</p>
-        <img class="edit-icon" src="/img/editar.png" data-id="${doc.id}" alt="Edit">
+        <img class="edit-icon" src="/img/lapiz.png" data-id="${doc.id}" alt="Edit">
         <img class="delete-icon" src="/img/eliminar.png" data-id="${doc.id}" alt="Delete">
         <span class="count-like" id="likes-count-${doc.id}">${doc.data().likes.length}</span>
         <img class="like-icon" src="/img/like.png" data-id="${doc.id}" alt="Like">
-        <p class="post-author">Compartido por: ${doc.data().user}</p>
       </div>
       `;
       publicationPost.append(post);
-        
-      //Este codigo es para que se elimine el post
+      // Este codigo es para que se elimine el post
       const btnDelete = publicationPost.querySelectorAll('.delete-icon');
       btnDelete.forEach((btn) => {
         btn.addEventListener('click', ({ target: { dataset } }) => {
-          if (doc.data().user === auth.currentUser.email)  {
-            // if (window.confirm('¿Estas segura de eliminar esta publicación?')) {
-            deletePost(dataset.id);
-          } else {
-            console.log('este post no es tuyo');
+          if (doc.data().user === auth.currentUser.email) {
+            if (window.confirm('¿Estas segura de eliminar esta publicación?')) {
+              deletePost(dataset.id);
+            } else {
+              console.log('este post no es tuyo');
             // alert ("No es posible eliminar este Post")
-          // }
-         }
-         });
-       });
-     });
-        //Aqui debemos crear el codigo para editar la publicacióm
-       const btnEdit = publicationPost.querySelectorAll('.edit-icon');
-       btnEdit.forEach((btn) => {
-       btn.addEventListener('click', async (e) => {
-       editingPostId = e.target.dataset.id;
-
-    try {
-      const tarea = await editpost(editingPostId);
-      console.log(editingPostId);
-      const postTitleElement = document.getElementById('postTitle');
-      postTitleElement.value = tarea.comment; 
-    } catch (error) {
-      console.error('Error al obtener el post', error);
-    }
-    });
-     });
-        
-     //este codigo es  para dar el like en el icono y q sea 1 por usuaria
-      const btnLike = publicationPost.querySelectorAll('.like-icon');
-      btnLike.forEach((btn) => {
-      btn.addEventListener('click', async ({ target: { dataset } }) => {
-      const postId = dataset.id;
-      await likePost(postId, auth.currentUser.email);
-     
-       });
-    });
-     // Actualizar la cantidad de likes en tiempo real en interfazz y en data
-        // Agrega un listener para escuchar el evento 'likeAdded'
-          document.addEventListener('likeAdded', () => {
-          const countLikes = publicationPost.querySelector(`#likes-count-${doc.id}`);
-          countLikes.textContent = doc.data().likes.length;
-        });     
+            }
+          }
+        });
       });
-    
-
-
+    });
+    // Aqui debemos crear el codigo para editar la publicacióm
+    const btnEdit = publicationPost.querySelectorAll('.edit-icon');
+    btnEdit.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        editingPostId = e.target.dataset.id;
+        try {
+          const tarea = await getDocument(editingPostId);
+          console.log(editingPostId);
+          const postTitleElement = document.getElementById('postTitle');
+          postTitleElement.value = tarea.comment;
+        } catch (error) {
+          console.error('Error al obtener el post', error);
+        }
+      });
+    });
+    // este codigo es  para dar el like en el icono y q sea 1 por usuaria
+    const btnLike = publicationPost.querySelectorAll('.like-icon');
+    btnLike.forEach((btn) => {
+      btn.addEventListener('click', async ({ target: { dataset } }) => {
+        const postId = dataset.id;
+        await likePost(postId, auth.currentUser.email);
+      });
+    });
+    // Actualizar la cantidad de likes en tiempo real en interfazz y en data
+    // Agrega un listener para escuchar el evento 'likeAdded'
+    document.addEventListener('likeAdded', () => {
+      const countLikes = publicationPost.querySelector(`#likes-count-${doc.id}`);
+      countLikes.textContent = doc.data().likes.length;
+    });
+  });
   const postContainerInner = document.createElement('div');
   postContainerInner.setAttribute('class', 'post-container-inner');
   postContainerInner.append(postTitle, postButton);
-
   postContainer.append(postContainerInner);
-
   section.append(title, buttonExit, imgMujeres, postContainer, publicationPost);
-
   return section;
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,22 +1,17 @@
-import {
-  db, 
-  collection, 
-  addDoc, 
-  getDocs, 
-  onSnapshot, 
-  query, 
-  orderBy,  
-  deleteDoc, 
-  getDoc, 
-  updateDoc
-
-} from '../firestore.js';
-
 import { doc } from 'firebase/firestore';
-
-
+import {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  orderBy,
+  deleteDoc,
+  getDoc,
+  updateDoc,
+} from '../firestore.js';
 const postCollection = collection(db, 'posts');
-
 export const addPost = (comment, user) => {
   addDoc(postCollection, {
     comment,
@@ -25,28 +20,24 @@ export const addPost = (comment, user) => {
     likes: [],
   });
 };
-
 export const querySnapshot = getDocs(postCollection);
-
 const q = query(postCollection, orderBy('date', 'desc'));
-
 export const paintRealTime = (callback) => onSnapshot(q, callback);
-
 export const deletePost = (id) => deleteDoc(doc(db, 'posts', id));
-
-export const editpost = async (id) => {
+export const getDocument = async (id) => {
   const docRef = doc(db, 'posts', id);
   const docSnap = await getDoc(docRef);
-
   if (docSnap.exists()) {
     return docSnap.data(); // Devolver los datos del documento
-  } else {
-    throw new Error('El documento no existe');
   }
+  throw new Error('El documento no existe');
 };
-
-
-// export const likePost = (id) => updateDoc (doc(db, 'posts', id));
+export const editPost = (documentId, comment) => {
+  const doclike = doc(db, 'posts', documentId);
+  updateDoc(doclike, {
+    comment,
+  });
+};
 export const likePost = async (documentId, userId) => {
   const doclike = doc(db, 'posts', documentId);
   try {
@@ -62,11 +53,11 @@ export const likePost = async (documentId, userId) => {
         const event = new Event('likeAdded');
         document.dispatchEvent(event);
       } else {
-        const dislike = likes.filter((like)=> like !== userId)
-        console.log("Deslikiaste")
-          
+        console.log('Ya habías dado like =).');
+        const dislike = likes.filter((like) => like !== userId);
+        console.log(dislike);
         await updateDoc(doclike, {
-          likes: dislike
+          likes: dislike,
         });
       }
     }
@@ -74,10 +65,3 @@ export const likePost = async (documentId, userId) => {
     console.error('Error al dar like :(', error);
   }
 };
-
-
-
-
-
-
-
